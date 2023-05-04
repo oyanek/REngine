8c52c2604ccbfc02125ca11544b33037b28e99d5
oyanek <oyanek@DESKTOP-FCVUNB5.localdomain>
2023-05-04 13:02:17 -0500

    added search by title and Id

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # REngine
 ## Movie Recommendation Engine in Python
 
+This is a hobby project to further my ML and stat knowledge. Also because I take entirely too long to pick out movies.
+
 Install and setup MongoDB Compass. Populate with csv files downloadable from [GroupLens][resourcelink].
 
 [resourcelink]:https://grouplens.org/datasets/movielens/
+
+To run scripts as of 5/4 
+~~~
+poetry run python script.py
+~~~
+
--- utils/search.py
+++ utils/search.py
@@ -6,17 +6,26 @@ client = MongoClient('localhost',27017)
 db = client['RENG']
 movies = db['movies']
 
-def search_title(movieId):
-    movie = movies.find_one({'movieId': movieId})
-    if movie:
-        title = movie['title']
-        print(f"Movie with ID {movieId} is '{title}'.")
+def search_title(movieId=None, title=None):
+    if movieId:
+        movie = movies.find_one({'movieId': movieId})
+        if movie:
+            title = movie['title']
+            print(f"Movie with ID {movieId} is '{title}'.")
+        else:
+            print(f"No movie found with ID {movieId}.")
+    elif title:
+        results = movies.find({'title': {'$regex': title, '$options': 'i'}})
+        for movie in results:
+            print(f"Movie with ID {movie['movieId']} is '{movie['title']}'.")
     else:
-        print(f"No movie found with ID {movieId}.")
+        print('Please provide either a movie ID or a title.')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Find a movie title by movieId.')
-    parser.add_argument('movie_id', type=int, help='The ID of the movie to search for')
+    parser = argparse.ArgumentParser(description='Find a movie by movieId or title.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i', '--movie_id', type=int, help='The ID of the movie to search for')
+    group.add_argument('-t', '--title', type=str, help='The title of the movie to search for')
     args = parser.parse_args()
 
-    search_title(args.movie_id)
+    search_title(args.movie_id, args.title)
